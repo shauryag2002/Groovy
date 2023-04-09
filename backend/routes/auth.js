99fd78801bb6a96897116c47f29ebd2e2b580b83
Shaurya Gupta <shauryag1000@gmail.com>
2023-04-09 15:45:16 +0530

    added try catch block for register route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,22 +3,26 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const router = require("express").Router();
 router.post("/register", async (req, res) => {
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
-  const username = await User.findOne({ username: req.body.username });
-  if (!username) {
-    if (!(await User.findOne({ email: req.body.email }))) {
-      const user = await new User({
-        ...req.body,
-        password: hashedPassword,
-      });
-      await user.save();
-      res.status(200).json({ user });
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const username = await User.findOne({ username: req.body.username });
+    if (!username) {
+      if (!(await User.findOne({ email: req.body.email }))) {
+        const user = await new User({
+          ...req.body,
+          password: hashedPassword,
+        });
+        await user.save();
+        res.status(200).json({ user });
+      } else {
+        res.status(200).json({ error: "email is allready taken." });
+      }
     } else {
-      res.status(200).json({ error: "email is allready taken." });
+      res.status(200).json({ error: "username is allready taken." });
     }
-  } else {
-    res.status(200).json({ error: "username is allready taken." });
+  } catch (err) {
+    console.log(err);
   }
 });
 router.post("/login", async (req, res) => {
